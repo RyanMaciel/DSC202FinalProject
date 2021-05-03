574a417026233789defea0d4641553a61dd31229
Nikola Danevski <ndanevsk@u.rochester.edu>
2021-05-04 00:03:42 +0200

    Cleaned bronze tables; initial EDA done. Presentation done

--- EDA.py
+++ EDA.py
@@ -12,6 +12,35 @@
 
 # COMMAND ----------
 
+# Draws a heatmap of the correlation between all the columns
+# and sorts the correlations of the columns with a SPECIFIC column_name 
+
+def plot_correlation(df):
+  import matplotlib.pyplot as plt
+  import seaborn as sns
+  import pandas as pd
+  
+  df_pd = df.toPandas()
+  df_pd.shape
+  
+  # Drop non-numeric columns (which is why the plot is smaller than 20x20)
+  corr = df_pd.corr()
+  ax = sns.heatmap(
+      corr, 
+      vmin=-1, vmax=1, center=0,
+      cmap=sns.diverging_palette(20, 120, n=100),
+      square=True
+  )
+  ax.set_xticklabels(
+      ax.get_xticklabels(),
+      rotation=45,
+      horizontalalignment='right'
+  );
+  
+  return corr
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## EDA consists of:
 # MAGIC * Arrival/Departure Delay Frequency
@@ -19,12 +48,21 @@
 # MAGIC * Average Arrival/Departure Delay per Airport
 # MAGIC * Average Arrival/Departure Delay per Day of Week
 # MAGIC * Average Arrival/Departure Delay per Day of Month
+# MAGIC * Correlation between Arrival Delay and the other variables
+# MAGIC * Correlation between Simplified Weather Data and Arrival/Departure Delay for NYC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT *
+# MAGIC FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3
+# MAGIC LIMIT 10
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC SELECT COUNT(*) AS FREQUENCY, ARR_DELAY
-# MAGIC FROM dscc202_group02_db.bronze_airports_cleaned
+# MAGIC FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3
 # MAGIC GROUP BY ARR_DELAY
 # MAGIC SORT BY ARR_DELAY
 
@@ -32,72 +70,68 @@
 
 # MAGIC %sql
 # MAGIC SELECT COUNT(*) AS FREQUENCY, DEP_DELAY
-# MAGIC FROM dscc202_group02_db.bronze_airports_cleaned
+# MAGIC FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3
 # MAGIC GROUP BY DEP_DELAY
 # MAGIC SORT BY DEP_DELAY
 
 # COMMAND ----------
 
-'''%sql
-SELECT AVG(ARR_DELAY) AS avg_arr_delay, AVG(DEP_DELAY) AS avg_dep_delay, MONTH
-FROM dscc202_group02_db.bronze_airports_cleaned
-GROUP BY MONTH
-SORT BY MONTH'''
+# MAGIC %sql
+# MAGIC SELECT AVG(ARR_DELAY) AS avg_arr_delay, AVG(DEP_DELAY) AS avg_dep_delay, DAY_OF_WEEK
+# MAGIC FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3
+# MAGIC GROUP BY DAY_OF_WEEK
+# MAGIC SORT BY DAY_OF_WEEK
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- https://dwgeek.com/spark-sql-date-and-timestamp-functions-and-examples.html/ -> to convert from TIMESTAMP to MONTH
+# MAGIC SELECT AVG(ARR_DELAY) AS avg_arr_delay, AVG(DEP_DELAY) AS avg_dep_delay, month(
+# MAGIC SCHEDULED_DEP_TIME) AS MONTH
+# MAGIC FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3
+# MAGIC GROUP BY MONTH
+# MAGIC SORT BY MONTH
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC SELECT AVG(ARR_DELAY) AS avg_arr_delay, AVG(DEP_DELAY) AS avg_dep_delay, ORIGIN
-# MAGIC FROM dscc202_group02_db.bronze_airports_cleaned
+# MAGIC FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3  
+# MAGIC WHERE ORIGIN IN ("JFK","SEA","BOS","ATL","LAX","SFO","DEN","DFW","ORD","CVG","CLT","DCA","IAH")
 # MAGIC GROUP BY ORIGIN
 # MAGIC SORT BY ORIGIN
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT AVG(ARR_DELAY) AS avg_arr_delay, AVG(DEP_DELAY) AS avg_dep_delay, DAY_OF_WEEK
-# MAGIC FROM dscc202_group02_db.bronze_airports_cleaned
-# MAGIC GROUP BY DAY_OF_WEEK
-# MAGIC SORT BY DAY_OF_WEEK
+# MAGIC SELECT AVG(ARR_DELAY) AS avg_arr_delay, AVG(DEP_DELAY) AS avg_dep_delay, dayofmonth(SCHEDULED_DEP_TIME) AS DAY_OF_MONTH
+# MAGIC FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3
+# MAGIC GROUP BY DAY_OF_MONTH
+# MAGIC SORT BY DAY_OF_MONTH
 
 # COMMAND ----------
 
-'''%sql
-SELECT AVG(ARR_DELAY) AS avg_arr_delay, AVG(DEP_DELAY) AS avg_dep_delay, DAY_OF_MONTH
-FROM dscc202_group02_db.bronze_airports_cleaned
-GROUP BY DAY_OF_MONTH
-SORT BY DAY_OF_MONTH'''
+flightDF = spark.sql("SELECT * FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3")
 
 # COMMAND ----------
 
-df = spark.sql("SELECT * FROM dscc202_group02_db.bronze_airports_cleaned")
+corr = plot_correlation(flightDF)
+corr["ARR_DELAY"].sort_values()
 
 # COMMAND ----------
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
+# Join Flight and Weather data for NYC and JFK based on year, month, day and hour.
+flight_and_weather_DF = spark.sql("""
+  SELECT * 
+  FROM dscc202_group02_db.bronze_air_traffic_cleaned_v3 F, dscc202_group02_db.bronze_weather_v1 W
+WHERE F.ORIGIN = "JFK" AND YEAR(F.SCHEDULED_DEP_TIME) = YEAR(W.time) AND MONTH(F.SCHEDULED_DEP_TIME) = MONTH(W.time) AND DAY(F.SCHEDULED_DEP_TIME) = DAY(W.time) AND HOUR(F.SCHEDULED_DEP_TIME) = HOUR(W.time)
+""")
 
 # COMMAND ----------
 
-df_pd = df.toPandas()
-df_pd.shape
+display(flight_and_weather_DF)
 
 # COMMAND ----------
 
-# Drop non-numeric columns (which is why the plot is smaller than 20x20)
-corr = df_pd.corr()
-ax = sns.heatmap(
-    corr, 
-    vmin=-1, vmax=1, center=0,
-    cmap=sns.diverging_palette(20, 120, n=100),
-    square=True
-)
-ax.set_xticklabels(
-    ax.get_xticklabels(),
-    rotation=45,
-    horizontalalignment='right'
-);
-
-# COMMAND ----------
-
+corr = plot_correlation(flight_and_weather_DF)
+corr["DEP_DELAY"].sort_values()
